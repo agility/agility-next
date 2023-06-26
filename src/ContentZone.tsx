e83d1f6a2659d11d23ddb8e99b2de6b077dd31ce
Joel Varty <joel@agilitycms.com>
2023-06-26 16:39:35 -0400

    fixed issue re-render when using useRouter #21

--- src/ContentZone.tsx
+++ src/ContentZone.tsx
@@ -1,45 +1,58 @@
-import React, { FC } from 'react';
-import { ContentZoneProps } from './types';
+import React, {FC} from "react"
+import {ContentZoneProps} from "./types"
 
+export const ContentZone: FC<ContentZoneProps> = ({
+	name,
+	page,
+	sitemapNode,
+	dynamicPageItem,
+	languageCode,
+	channelName,
+	getModule,
+	isDevelopmentMode,
+	isPreview,
+	globalData,
+}) => {
+	if (!page) return null
 
-export const ContentZone:FC<ContentZoneProps> = ({ name, page, sitemapNode, dynamicPageItem, languageCode, channelName, getModule, isDevelopmentMode, isPreview, globalData }) => {
-
-	const RenderModules = () => {
-		if (!page) return null
-
-		let modules = page.zones[name];
-
-		const modulesToRender = modules.map(m => {
-
-			const AgilityModule = getModule(m.moduleName)
-
-			let props = {
-				 page,
-				 sitemapNode,
-				 dynamicPageItem,
-				 module: m.item,
-				 languageCode,
-				 channelName,
-				 customData: m.customData || null,
-				 isDevelopmentMode,
-				 isPreview,
-				 globalData: globalData
-
-			}
-
-			if (AgilityModule) {
-				return <AgilityModule  key={m.item.contentID} { ... props} />
-			} else {
-				throw new Error(`React Component for ${m.moduleName} was not found in the Agility Modules list.`)
-			}
-
-		})
-
-		return modulesToRender;
-	}
-
+	const modules = page.zones[name]
 
 	return (
-		<RenderModules />
+		<>
+			{modules.map((m) => {
+				const AgilityModule = getModule(m.moduleName)
+
+				let props = {
+					page,
+					sitemapNode,
+					dynamicPageItem,
+					module: m.item,
+					languageCode,
+					channelName,
+					customData: m.customData || null,
+					isDevelopmentMode,
+					isPreview,
+					globalData: globalData,
+				}
+
+				if (AgilityModule) {
+					return <AgilityModule key={m.item.contentID} {...props} />
+				} else {
+					if (isPreview || isDevelopmentMode) {
+						return (
+							<div>
+								The component for{" "}
+								<em>
+									<strong>{m.moduleName}</strong>
+								</em>{" "}
+								was not found in the Agility Modules list.
+							</div>
+						)
+					}
+
+					throw new Error(`Component for ${m.moduleName} was not found in the Agility Modules list.`)
+				}
+			})}
+		</>
 	)
 }
